a11f50e8818fe29a908876910167c65d920faa8e
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-07-03 10:36:41 -0400

    Adding sync-prod script

---
+++ bin/sync-prod.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# download the prod db
+drush sql:sync @execed.master @execed._local --structure-tables-list=cache,cache_*
+
+drush cr
+
+drush sql:sanitize -y
+
+drush uli
+
+# check whether to keep the prod config in place
+while :; do
+  case $1 in
+    -c|--config) CONFIG="YES"
+    ;;
+    *) break
+  esac
+  shift
+done
+
+if [ "$CONFIG" != "YES" ]; then
+  # import local config
+  drush cim -y
+  drush csim -y
+fi
+
+echo "Sync complete."
