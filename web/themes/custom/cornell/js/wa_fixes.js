a5674808e6469f16846ee46dd4c5d02f2e53e8e5
Khaled Arnaout <ka368@cornell.edu>
2019-07-31 16:35:18 -0400

    fixed accessibility issues with the site

---
+++ web/themes/custom/cornell/js/wa_fixes.js
@@ -0,0 +1,18 @@
+( function( $ ) {
+    $(document).ready(function (){
+      // add Event listener to hide form (chat with us) fields when window is not popped up.
+      function toggleDisplayNone() {
+        if ( $('div.chat-bubble').hasClass('open')){
+          $('.chat-bubble__popup button').css('display','block');
+          $('.chat-bubble__popup div.chat-bubble__popup-content').css('display','block');
+        }
+      }
+      document.querySelector('div.chat-bubble button.chat-bubble__trigger').addEventListener("click", toggleDisplayNone, false);
+
+      // hide arrow from site nave elements that don't have dropdowns
+      let navElementsArray = $('.site-nav__item').not($('.site-nav__item').has('ul.site-nav__secondary'));
+      for(let i=0; i<navElementsArray.length;i++){
+        navElementsArray.children().after('');
+      }
+    });
+} )( jQuery );
